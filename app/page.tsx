a888d2b212a93f48c4732572df3bdcb70d99a5df
ABDULQUADIRL <elanquad92@gmail.com>
2025-12-14 09:55:08 +0100

    update homepage

--- app/page.tsx
+++ app/page.tsx
@@ -83,8 +83,6 @@ export default async function Page() {
   return (
     <div className="min-h-screen">
     <main className="w-[85vw] mx-auto px-4 sm:px-6 lg:px-8 py-10">
-        {/* <h1 className="pt-6">{homes.data.title}</h1>
-        <p>{homes.data.description}</p> */}
         <HeroSection data={homes.data.blocks[0]} />
         <ServiceSection data={homes.data.blocks[1]} />
         <News articles={articles} />
